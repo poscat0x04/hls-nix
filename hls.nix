1ed0bd962801932734496449d5b75d371d3b1377
Poscat <poscat@mail.poscat.moe>
2020-06-16 00:49:31 +0800

    Parameterize source

--- hls.nix
+++ hls.nix
@@ -8,7 +8,7 @@ let
   hsPkgs = with pkgs.haskell-nix;
     stackProject {
       src = cleanSourceHaskell {
-        src = source.hls-master;
+        src = source."hls-${tag}";
         name = "hls-source";
       };
       cache = l (./cache + "-${tag}/${version}.json");
